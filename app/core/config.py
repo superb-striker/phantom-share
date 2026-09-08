54ca07ea6342a4444b1e6c74a3ec6c73fba1b2f9
Nipun Kothari <nipunkothari04@gmail.com>
2026-09-08 17:56:37 +0530

    remove deprecated class and ensure that it doesn't load extra env vars specified in .env for docker compose

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,6 @@
 from functools import lru_cache
 from typing import List, ClassVar
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -58,9 +58,11 @@ class Settings(BaseSettings):
     # CORS
     CORS_ORIGINS: List[str] 
  
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+    model_config = SettingsConfigDict(
+        env_file = ".env",
+        env_file_encoding = "utf-8",
+        extra = "ignore"
+    )
 
 @lru_cache()
 def get_settings() -> Settings:
